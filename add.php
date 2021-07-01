2a71dad673c4b22e7d055d555bc1dfbf2f96c6c1
alamehor <alamehor@yahoo.com>
2021-07-01 08:16:48 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,12 +1,10 @@
  <?php
-//main signature plugin file - without table wp_signature & wp_signature_variables - to do
+//manage request from ajax
 //include wp-config or wp-load.php
 $root = dirname(dirname(dirname(dirname(__FILE__))));
 if (file_exists($root . '/wp-load.php')) {
-    // WP 2.6
     require_once $root . '/wp-load.php';
 } else {
-    // Before 2.6
     require_once $root . '/wp-config.php';
 }
 
